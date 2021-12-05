a83b3de2357ea72e153283c188f8a10f788be701
MatiasLeonMdz <95253340+MatiasLeonMdz@users.noreply.github.com>
2021-12-05 02:22:23 -0300

    Initialize redux

---
+++ src/components/ReduxPage/ReduxPageContainer.js
@@ -0,0 +1,16 @@
+import { connect } from "react-redux";
+import ReduxPage from "./ReduxPage.js";
+import * as Actions from "../../actions/actions";
+
+function mapStateToProps(state) {
+  return {
+    counter: state.counter
+  };
+}
+
+const mapDispatchToProps = {
+  increment: Actions.increment,
+  decrement: Actions.decrement
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(ReduxPage);
